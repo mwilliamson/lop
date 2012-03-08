6b6280b406e64cbe9687fa54bd28cee77b73162d
Michael Williamson <mike@zwobble.org>
2012-03-08 20:17:27 +0000

    Export parsing results

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.Parser = require("./lib/parser").Parser;
 exports.rules = require("./lib/rules");
 exports.testing = require("./lib/testing");
 exports.errors = require("./lib/errors");
+exports.results = require("./lib/parsing-results");
