40df55f25078ae67d36841a356fb5b638b8abe7d
Michael Williamson <mike@zwobble.org>
2012-03-31 14:37:42 +0100

    Expose StringSource

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.rules = require("./lib/rules");
 exports.testing = require("./lib/testing");
 exports.errors = require("./lib/errors");
 exports.results = require("./lib/parsing-results");
+exports.StringSource = require("./lib/StringSource");
