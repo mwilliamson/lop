75074cc1f6fbe14e8057b1857a4bac6f2739bdc6
Michael Williamson <mike@zwobble.org>
2012-07-30 10:26:13 +0100

    Add right association to bottom up parsing

--- test/bottom-up.test.js
+++ test/bottom-up.test.js
@@ -111,7 +111,6 @@ exports.parsingStopsIfPrefixRuleFails = function(test) {
     test.done();
 };
 
-
 exports.canParseExpressionWithTwoLeftAssociativeOperators = function(test) {
     var partialAddRule = lazyRule(function() {
         var right = rules.sequence.capture(expressionParser.leftAssociative("add"), "right");
@@ -162,6 +161,60 @@ exports.canParseExpressionWithTwoLeftAssociativeOperators = function(test) {
     assertIsSuccess(test, result, {
         value: ["+", ["*", ["*", "1", "2"], "3"], ["*", "4", "5"]],
         source: source("1 * 2 * 3 + 4 * 5", 0, 17)
+    });
+    test.done();
+};
+
+exports.canParseExpressionWithRightAssociativeOperators = function(test) {
+    var partialAddRule = lazyRule(function() {
+        var right = rules.sequence.capture(expressionParser.leftAssociative("add"));
+        return rules.sequence(
+            rules.token("symbol", "+"),
+            right
+        ).map(function(right) {
+            return function(left) {
+                return ["+", left, right];
+            };
+        });
+    });
+    
+    var partialPowerRule = lazyRule(function() {
+        var right = rules.sequence.capture(expressionParser.rightAssociative("power"));
+        return rules.sequence(
+            rules.token("symbol", "^"),
+            right
+        ).map(function(right) {
+            return function(left) {
+                return ["^", left, right];
+            };
+        });
+    });
+    
+    var expressionParser = bottomUp.parser("expression",
+        [rules.tokenOfType("number")],
+        [
+            bottomUp.infix("power", partialPowerRule),
+            bottomUp.infix("add", partialAddRule)
+        ]
+    );
+    
+    var rule = expressionParser.rule();
+    
+    var result = parse(rule, [
+        token("number", "1", source("1 ^ 2 ^ 3 + 4 ^ 5", 0, 1)),
+        token("symbol", "^", source("1 ^ 2 ^ 3 + 4 ^ 5", 2, 3)),
+        token("number", "2", source("1 ^ 2 ^ 3 + 4 ^ 5", 4, 5)),
+        token("symbol", "^", source("1 ^ 2 ^ 3 + 4 ^ 5", 6, 7)),
+        token("number", "3", source("1 ^ 2 ^ 3 + 4 ^ 5", 8, 9)),
+        token("symbol", "+", source("1 ^ 2 ^ 3 + 4 ^ 5", 10, 11)),
+        token("number", "4", source("1 ^ 2 ^ 3 + 4 ^ 5", 12, 13)),
+        token("symbol", "^", source("1 ^ 2 ^ 3 + 4 ^ 5", 14, 15)),
+        token("number", "5", source("1 ^ 2 ^ 3 + 4 ^ 5", 16, 17)),
+        token("end", null, source("1 ^ 2 ^ 3 + 4 ^ 5", 17, 17))
+    ]);
+    assertIsSuccess(test, result, {
+        value: ["+", ["^", "1", ["^", "2", "3"]], ["^", "4", "5"]],
+        source: source("1 ^ 2 ^ 3 + 4 ^ 5", 0, 17)
     });
     test.done();
 };
